af55bb946eeae9605393fdb9bd44b55356e1a30b
considerate-template-4770 <rafaelb24@yahoo.com>
2018-09-28 00:20:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,8 +8,7 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-#display_board(board)
+display_board(board)
 
-puts "X #{board[5]}"
-
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
